22c4b644a25fdf3c75e51f3b80d21c52e4d96019
Ahmad-Hazrati <hazrati.ahmad@gmail.com>
2024-09-25 20:57:22 +0200

    Add PostList generic view

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from .views import home
+from . import views
 
 urlpatterns = [
-    path('', home, name='home'),
-    
+    path('', views.PostList.as_view(), name='home'),
 ]
